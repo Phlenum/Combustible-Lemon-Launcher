5583c857ce1ca86a515f0da52864bc98174eacc1
iBuilder99 <meyer.julianmeyer.julian@googlemail.com>
2013-08-05 13:11:04 +0200

    renaming harvested lemon leaves

--- common/ibuilder99/mods/cll/blocks/LemonLauncherBlocks.java
+++ common/ibuilder99/mods/cll/blocks/LemonLauncherBlocks.java
@@ -14,7 +14,7 @@ public class LemonLauncherBlocks {
 	
 	public static void initBlocks(){
 		LemonLeaves = new LemonLeaves(ConfigLoader.LemonLeavesID, Reference.BLOCK_LEMON_LEAVES, "Lemon Leaves", 0.2F, 0.2F, Block.soundGrassFootstep);
-		LemonLeavesHarvested = new LemonLeavesHarvested(ConfigLoader.LemonLeavesHarvestedID, Reference.BLOCK_LEMON_LEAVES_HARVESTED, "Lemon Leaves (Growing)", 0.2F, 0.2F, Block.soundGrassFootstep);
+		LemonLeavesHarvested = new LemonLeavesHarvested(ConfigLoader.LemonLeavesHarvestedID, Reference.BLOCK_LEMON_LEAVES_HARVESTED, "Lemon Leaves (Harvested)", 0.2F, 0.2F, Block.soundGrassFootstep);
 		LemonSapling = new LemonSapling(ConfigLoader.LemonSapling, Reference.BLOCK_LEMON_SAPLING, "Lemon Sapling", Block.soundGrassFootstep, 0.0F, 0.0F);
 		
 		BlueGel = new BlueGel(ConfigLoader.BlueGelID, Reference.BLOCK_BLUE_GEL, "Blue Gel");
