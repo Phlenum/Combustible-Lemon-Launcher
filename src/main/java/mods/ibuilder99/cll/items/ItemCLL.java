9085f78d62962ec15f1b9765b423e6e4bab1ade8
PhilJulian <meyer.julianmeyer.julian@googlemail.com>
2014-01-04 01:03:45 +0100

    ItemCLL - Item base class

---
+++ src/main/java/mods/ibuilder99/cll/items/ItemCLL.java
@@ -0,0 +1,23 @@
+package mods.ibuilder99.cll.items;
+
+import mods.ibuilder99.cll.CombustibleLemonLauncher;
+import mods.ibuilder99.cll.lib.Reference;
+import net.minecraft.item.Item;
+
+/**
+ * Combustible Lemon Launcher
+ * Copyright (c) 2014 Phil Julian
+ * @author Phil Julian (aka iBuilder99)
+ */
+
+public class ItemCLL extends Item {
+	
+	public ItemCLL(int id, String unloc, String en_US){
+		super();
+		setUnlocalizedName(unloc);
+		setTextureName(Reference.TEXTURE_PREFIX + unloc);
+		setCreativeTab(CombustibleLemonLauncher.tabCombustibleLemonLauncher);
+		field_150901_e.func_148756_a(id, unloc, this);
+	}
+	
+}
--- src/main/java/mods/ibuilder99/cll/lib/Reference.java
+++ src/main/java/mods/ibuilder99/cll/lib/Reference.java
@@ -14,6 +14,7 @@ public class Reference {
 	public static final String MOD_NAME = "Combustible Lemon Launcher";
 	public static final String MOD_ID = "CLL";
 	public static final String MOD_VERSION = "2.0.001";
+	public static final String TEXTURE_PREFIX = Reference.MOD_ID.toLowerCase() + ":";
 
 	public static final String ITEM_LEMON = "Lemon";
 	public static final String ITEM_LEMON_EXPLOSIVE = "LemonExplosive";
