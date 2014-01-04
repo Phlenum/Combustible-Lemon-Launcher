292ea88f06d01dd835051e3100762a3bddf8ce41
PhilJulian <meyer.julianmeyer.julian@googlemail.com>
2014-01-04 18:36:38 +0100

    Remove unused import in main mod file

--- src/main/java/mods/ibuilder99/cll/CombustibleLemonLauncher.java
+++ src/main/java/mods/ibuilder99/cll/CombustibleLemonLauncher.java
@@ -12,7 +12,6 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.registry.LanguageRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
