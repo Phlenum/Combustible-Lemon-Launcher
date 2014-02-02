fa939b8ed805e176cce253336eecd1312faf486d
PhilJulian <julian.phil.meyer@gmail.com>
2014-02-02 19:16:59 +0100

    Apply ItemRenderer to ItemCombustibleLemonLauncher

--- src/main/java/mods/ibuilder99/cll/client/RenderItemCombustibleLemonLauncher.java
+++ src/main/java/mods/ibuilder99/cll/client/RenderItemCombustibleLemonLauncher.java
@@ -1,11 +1,14 @@
 package mods.ibuilder99.cll.client;
 
+import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import mods.ibuilder99.cll.lib.Reference;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.IItemRenderer;
-
+import net.minecraftforge.client.model.obj.WavefrontObject;
 import static org.lwjgl.opengl.GL11.*;
-// Vertex Buffer Objects
-import static org.lwjgl.opengl.GL15.*;
 
 /**
  * Combustible Lemon Launcher
@@ -13,33 +16,18 @@ import static org.lwjgl.opengl.GL15.*;
  * @author Phil Julian (aka iBuilder99)
  */
 
+@SideOnly(Side.CLIENT)
 public class RenderItemCombustibleLemonLauncher implements IItemRenderer {
 
-	/**
-	 * One Model - one (static) instance of a model loader
-	 * <p>Featuring OpenGL 1.5 Vertex Buffer Objects!</p>
-	 */
+	private static WavefrontObject model_CombustibleLemonLauncher;
+	private static final ResourceLocation texture_CombustibleLemonLauncher = new ResourceLocation(Reference.TEXTURE_COMBUSTIBLE_LEMON_LAUNCHER);
 	
-	public static class Model_CombustibleLemonLauncher {
-		
-		private static int vertexBuffer;
-		
-		public static void loadModel(){
-			// TODO Parse .obj File and send its data to OpenGL
-			vertexBuffer = glGenBuffers();
-		}
-		
-		public static void renderModel(){
-			glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-			// TODO Draw Arrays
-			glBindBuffer(GL_ARRAY_BUFFER, 0);
-		}
-		
-		public static void deleteBuffer(){
-			glDeleteBuffers(vertexBuffer);
-			// TODO Delete buffered Data from (V-)RAM
-		}
-		
+	public static void initialize(){
+		model_CombustibleLemonLauncher = new WavefrontObject(new ResourceLocation(Reference.MODEL_COMBUSTIBLE_LEMON_LAUNCHER));
+	}
+	
+	private static void bindTexture(){
+		FMLClientHandler.instance().getClient().renderEngine.bindTexture(texture_CombustibleLemonLauncher);
 	}
 	
 	@Override
@@ -57,28 +45,29 @@ public class RenderItemCombustibleLemonLauncher implements IItemRenderer {
 		glPushMatrix();
 		switch(type){
 		case ENTITY:
-			glTranslatef(0.0F, 0.0F, 0.0F);
-			glRotatef(0.0F, 1.0F, 0.0F, 0.0F);
-			glRotatef(0.0F, 0.0F, 1.0F, 0.0F);
-			glRotatef(0.0F, 0.0F, 0.0F, 1.0F);
-			glScalef(1.0F, 1.0F, 1.0F);
-			Model_CombustibleLemonLauncher.renderModel();
+			glTranslatef(0.0F, 0.2F, 0.0F);
+			glScalef(0.32F, 0.32F, 0.32F);
+			glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+			bindTexture();
+			model_CombustibleLemonLauncher.renderAll();
 			break;
 		case EQUIPPED:
-			glTranslatef(0.0F, 0.0F, 0.0F);
-			glRotatef(0.0F, 1.0F, 0.0F, 0.0F);
-			glRotatef(0.0F, 0.0F, 1.0F, 0.0F);
-			glRotatef(0.0F, 0.0F, 0.0F, 1.0F);
-			glScalef(1.0F, 1.0F, 1.0F);
-			Model_CombustibleLemonLauncher.renderModel();
+			glTranslatef(0.7F, 0.5F, 0.7F);
+			glRotatef(135.0F, 0.0F, 1.0F, 0.0F);
+			glRotatef(-60.0F, 0.0F, 0.0F, 1.0F);
+			glScalef(0.5F, 0.5F, 0.5F);
+			glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+			bindTexture();
+			model_CombustibleLemonLauncher.renderAll();
 			break;
 		case EQUIPPED_FIRST_PERSON:
-			glTranslatef(0.0F, 0.0F, 0.0F);
-			glRotatef(0.0F, 1.0F, 0.0F, 0.0F);
-			glRotatef(0.0F, 0.0F, 1.0F, 0.0F);
-			glRotatef(0.0F, 0.0F, 0.0F, 1.0F);
-			glScalef(1.0F, 1.0F, 1.0F);
-			Model_CombustibleLemonLauncher.renderModel();
+			glTranslatef(0.5F, 0.5F, 0.5F);
+			glRotatef(5.0F, 1.0F, 0.0F, 0.0F);
+			glRotatef(50.0F, 0.0F, 1.0F, 0.0F);
+			glScalef(0.5F, 0.5F, 0.5F);
+			glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+			bindTexture();
+			model_CombustibleLemonLauncher.renderAll();
 			break;
 		case FIRST_PERSON_MAP:
 			glTranslatef(0.0F, 0.0F, 0.0F);
@@ -86,15 +75,15 @@ public class RenderItemCombustibleLemonLauncher implements IItemRenderer {
 			glRotatef(0.0F, 0.0F, 1.0F, 0.0F);
 			glRotatef(0.0F, 0.0F, 0.0F, 1.0F);
 			glScalef(1.0F, 1.0F, 1.0F);
-			Model_CombustibleLemonLauncher.renderModel();
+			glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+			bindTexture();
+			model_CombustibleLemonLauncher.renderAll();
 			break;
 		case INVENTORY:
-			glTranslatef(0.0F, 0.0F, 0.0F);
-			glRotatef(0.0F, 1.0F, 0.0F, 0.0F);
-			glRotatef(0.0F, 0.0F, 1.0F, 0.0F);
-			glRotatef(0.0F, 0.0F, 0.0F, 1.0F);
-			glScalef(1.0F, 1.0F, 1.0F);
-			Model_CombustibleLemonLauncher.renderModel();
+			glScalef(0.32F, 0.32F, 0.32F);
+			glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+			bindTexture();
+			model_CombustibleLemonLauncher.renderAll();
 			break;
 		default:
 			break;
--- src/main/java/mods/ibuilder99/cll/proxy/ClientProxy.java
+++ src/main/java/mods/ibuilder99/cll/proxy/ClientProxy.java
@@ -1,5 +1,8 @@
 package mods.ibuilder99.cll.proxy;
 
+import net.minecraftforge.client.MinecraftForgeClient;
+import mods.ibuilder99.cll.client.RenderItemCombustibleLemonLauncher;
+
 /**
  * Combustible Lemon Launcher
  * Copyright (c) 2014 Phil Julian
@@ -9,8 +12,9 @@ package mods.ibuilder99.cll.proxy;
 public class ClientProxy extends CommonProxy {
 	
 	@Override
-	public void initializeRenderers() {
-		//TODO: Initialize custom model
+	public void initializeRenderers(){
+		RenderItemCombustibleLemonLauncher.initialize();
+		MinecraftForgeClient.registerItemRenderer(itemCombustibleLemonLauncher, new RenderItemCombustibleLemonLauncher());
 	}
 
 }
