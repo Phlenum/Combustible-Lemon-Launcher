5c37cd2b30721c45fe9cf2f53e5acae21cf94d3e
PhilJulian <julian.phil.meyer@gmail.com>
2014-02-04 21:15:46 +0100

    Only register the KeyBinding stuff it will be used

--- src/main/java/mods/ibuilder99/cll/proxy/ClientProxy.java
+++ src/main/java/mods/ibuilder99/cll/proxy/ClientProxy.java
@@ -3,6 +3,7 @@ package mods.ibuilder99.cll.proxy;
 import cpw.mods.fml.common.FMLCommonHandler;
 import net.minecraftforge.client.MinecraftForgeClient;
 import mods.ibuilder99.cll.client.RenderItemCombustibleLemonLauncher;
+import mods.ibuilder99.cll.lib.CLLConfiguration;
 import mods.ibuilder99.cll.lib.CLLKeyBindingHandler;
 
 /**
@@ -21,7 +22,9 @@ public class ClientProxy extends CommonProxy {
 	
 	@Override
 	public void initializeKeyBinding(){
-		FMLCommonHandler.instance().bus().register (new CLLKeyBindingHandler());
+		if(CLLConfiguration.BO_useKeyToFire){
+			FMLCommonHandler.instance().bus().register(new CLLKeyBindingHandler());
+		}
 	}
 
 }
