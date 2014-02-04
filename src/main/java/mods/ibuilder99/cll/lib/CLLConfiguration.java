1d1538d233f4637d42d98af81401d106157fbfee
PhilJulian <julian.phil.meyer@gmail.com>
2014-02-04 18:20:55 +0100

    Configuration entry for the firing key

--- src/main/java/mods/ibuilder99/cll/lib/CLLConfiguration.java
+++ src/main/java/mods/ibuilder99/cll/lib/CLLConfiguration.java
@@ -2,6 +2,8 @@ package mods.ibuilder99.cll.lib;
 
 import java.io.File;
 
+import org.lwjgl.input.Keyboard;
+
 import net.minecraftforge.common.config.Configuration;
 import static net.minecraftforge.common.config.Configuration.CATEGORY_GENERAL;
 
@@ -14,11 +16,16 @@ import static net.minecraftforge.common.config.Configuration.CATEGORY_GENERAL;
 public class CLLConfiguration {
 	
 	public static boolean BO_useKeyToFire;
+	public static int INT_keyForFiring;
 	
 	public static void initializeConfiguration(File configFile){
 		Configuration configObj = new Configuration(configFile);
 		
 		BO_useKeyToFire = configObj.get(CATEGORY_GENERAL, "useKeyToFire", false).getBoolean(false);
+		configObj.addCustomCategoryComment(CATEGORY_GENERAL, 
+				"Use the LWJGL Keyboard Codes to configure\n"
+				+ "http://minecraft.gamepedia.com/Key_Codes");
+		INT_keyForFiring = configObj.get(CATEGORY_GENERAL, "keyForFiring", Keyboard.KEY_F).getInt();
 		
 		if(configObj.hasChanged()){
 			configObj.save();
