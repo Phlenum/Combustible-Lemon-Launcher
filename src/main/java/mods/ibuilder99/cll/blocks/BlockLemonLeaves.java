95f1c2610c93f7c172624182e9d323a5bccf071e
PhilJulian <julian.phil.meyer@gmail.com>
2014-02-15 13:42:48 +0100

    Let the Leaf blocks drop items with a random chance

--- src/main/java/mods/ibuilder99/cll/blocks/BlockLemonLeaves.java
+++ src/main/java/mods/ibuilder99/cll/blocks/BlockLemonLeaves.java
@@ -1,10 +1,13 @@
 package mods.ibuilder99.cll.blocks;
 
+import java.util.Random;
 import cpw.mods.fml.common.registry.GameRegistry;
 import mods.ibuilder99.cll.CombustibleLemonLauncher;
 import mods.ibuilder99.cll.lib.Reference;
+import mods.ibuilder99.cll.proxy.CommonProxy;
 import net.minecraft.block.BlockLeavesBase;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
 
 /**
  * Combustible Lemon Launcher
@@ -15,11 +18,24 @@ public class BlockLemonLeaves extends BlockLeavesBase {
 
 	public BlockLemonLeaves(int id, String unloc, float hardness, float resistance, SoundType sound){
 		super(Material.leaves, true);
-		setStepSound(sound);
 		setBlockName(unloc);
+		setHardness(hardness);
+		setResistance(resistance);
+		setStepSound(sound);
 		setBlockTextureName(Reference.TEXTURE_PREFIX + unloc);
 		setCreativeTab(CombustibleLemonLauncher.TAB_COMBUSTIBLE_LEMON_LAUNCHER);
 		GameRegistry.registerBlock(this, unloc);
+	}
+	
+	@Override
+	public Item getItemDropped(int par1, Random rand, int par2){
+		return CommonProxy.itemLemon;
+	}
+	
+	@Override
+	public int quantityDropped(Random rand){
+		int randomChance = rand.nextInt(5);
+		return (randomChance == 3 ? 2 : 1);
 	}
 
 }
--- src/main/java/mods/ibuilder99/cll/blocks/BlockLemonLeavesHarvested.java
+++ src/main/java/mods/ibuilder99/cll/blocks/BlockLemonLeavesHarvested.java
@@ -1,7 +1,9 @@
 package mods.ibuilder99.cll.blocks;
 
+import java.util.Random;
 import mods.ibuilder99.cll.CombustibleLemonLauncher;
 import mods.ibuilder99.cll.lib.Reference;
+import net.minecraft.item.Item;
 import net.minecraft.block.BlockLeavesBase;
 import net.minecraft.block.material.Material;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -15,11 +17,18 @@ public class BlockLemonLeavesHarvested extends BlockLeavesBase {
 
 	public BlockLemonLeavesHarvested(int id, String unloc, float hardness, float resistance, SoundType sound){
 		super(Material.leaves, true);
-		setStepSound(sound);
 		setBlockName(unloc);
+		setHardness(hardness);
+		setResistance(resistance);
+		setStepSound(sound);
 		setBlockTextureName(Reference.TEXTURE_PREFIX + unloc);
 		setCreativeTab(CombustibleLemonLauncher.TAB_COMBUSTIBLE_LEMON_LAUNCHER);
 		GameRegistry.registerBlock(this, unloc);
+	}
+	
+	@Override
+	public Item getItemDropped(int par1, Random rand, int par2){
+		return null;
 	}
 
 }
