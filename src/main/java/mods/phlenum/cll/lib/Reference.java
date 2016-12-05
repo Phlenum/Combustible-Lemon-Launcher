63583a9e2c5d4e52fafe6ea44e52b85f826d2d90
Phil Julian <julian.phil.meyer@gmail.com>
2016-12-05 18:34:47 +0100

    Using underscore names

--- src/main/java/mods/phlenum/cll/lib/Reference.java
+++ src/main/java/mods/phlenum/cll/lib/Reference.java
@@ -18,25 +18,25 @@ public final class Reference {
 	public static final String TEXTURE_PREFIX = Reference.MOD_ID + ":";
 
 	public static final String ITEM_LEMON = "lemon";
-	public static final String ITEM_LEMON_EXPLOSIVE = "lemonexplosive";
+	public static final String ITEM_LEMON_EXPLOSIVE = "lemon_explosive";
 	public static final String ITEM_COMBUSTIBLE_LEMON_LAUNCHER = "combustiblelemonlauncher";
 	
 	public static final String OREDICT_LEMON = "foodLemon";
 
-	public static final String BLOCK_LEMON_LEAVES = "lemonleaves";
-	public static final String BLOCK_LEMON_LEAVES_HARVESTED = "lemonleavesharvested";
-	public static final String BLOCK_LEMON_TREE_LOG = "lemontreelog";
-	public static final String BLOCK_LEMON_TREE_PLANKS = "lemontreeplanks";
-	public static final String BLOCK_LEMON_TREE_SAPLING = "lemontreesapling";
+	public static final String BLOCK_LEMON_LEAVES = "lemon_leaves";
+	public static final String BLOCK_LEMON_LEAVES_HARVESTED = "lemon_leaves_harvested";
+	public static final String BLOCK_LEMON_TREE_LOG = "lemon_tree_log";
+	public static final String BLOCK_LEMON_TREE_PLANKS = "lemon_tree_planks";
+	public static final String BLOCK_LEMON_TREE_SAPLING = "lemon_tree_sapling";
 	
 	public static final String SOUNDEVENT_COMBUSTIBLELEMONLAUNCHER_FIRE = "item.combustiblelemonlauncher.fire";
 	public static final String SOUNDEVENT_COMBUSTIBLELEMONLAUNCHER_OUTOFAMMO = "item.combustiblelemonlauncher.outofammo";
 
-	public static final String MODEL_COMBUSTIBLE_LEMON_LAUNCHER = MOD_ID.toLowerCase() + ":models/combustiblelemonlauncher.obj";
-	public static final String TEXTURE_COMBUSTIBLE_LEMON_LAUNCHER = MOD_ID.toLowerCase() + ":textures/models/combustiblelemonlauncher.png";
+	public static final String MODEL_COMBUSTIBLE_LEMON_LAUNCHER = MOD_ID.toLowerCase() + ":models/combustible_lemon_launcher.obj";
+	public static final String TEXTURE_COMBUSTIBLE_LEMON_LAUNCHER = MOD_ID.toLowerCase() + ":textures/models/combustible_lemon_launcher.png";
 
-	public static final String ENTITY_LEMON = MOD_ID.toLowerCase() + ":entitylemon";
-	public static final String DAMAGE_SOURCE_EXPLOSIVE_LEMON = "explosivelemon";
+	public static final String ENTITY_LEMON = MOD_ID.toLowerCase() + ":entity_lemon";
+	public static final String DAMAGE_SOURCE_EXPLOSIVE_LEMON = "explosive_lemon";
 	
 	public static final String CONFIG_GENERATE_TREES = "Generate lemon trees";
 
